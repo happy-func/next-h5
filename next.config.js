1473bc56349c9d11559d822fc9009609a756f79a
happy-func <stevenhanth@gmail.com>
2021-04-08 21:57:16 +0800

    尝试优化首屏

--- next.config.js
+++ next.config.js
@@ -1,3 +1,13 @@
 module.exports = {
     useFileSystemPublicRoutes: false,
+    webpack(config, { isServer }) {
+        console.log(isServer)
+        if (!isServer) {
+            // config.externals = {
+            //     '@material-ui/core': 'material-ui'
+            // }
+        }
+        return config;
+    },
+
 }
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
 import React from "react";
+import { Button } from '@material-ui/core';
 import style from '../styles/Home.module.css';
 
 function Home({ list }) {
     return (
         <div>
+            <Button color='primary'>按钮</Button>
             {list.map(item => <p key={item} className={style.text}>test {item}</p>)}
         </div>
     )
