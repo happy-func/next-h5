912dfaed4a051563d19087fc31bf262ca3dd236f
happy-func <stevenhanth@gmail.com>
2021-04-12 21:54:16 +0800

    swiper

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,30 @@
 import React from "react";
 import style from '../styles/Home.module.css';
 import BaseLink from "@/components/BaseLink";
+import SwiperCore, { Autoplay } from 'swiper';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/swiper.min.css';
+SwiperCore.use([Autoplay]);
 
 function Home({ list }) {
     return (
         <div className={style.page}>
             <BaseLink src="/test?id=2">test</BaseLink>
+            <Swiper
+              spaceBetween={50}
+              onSlideChange={() => console.log('slide change')}
+              onSwiper={(swiper) => console.log(swiper)}
+              centeredSlides={true}
+              autoplay={{
+                delay: 2500,
+                disableOnInteraction: false,
+              }}
+            >
+                <SwiperSlide>Slide 1</SwiperSlide>
+                <SwiperSlide>Slide 2</SwiperSlide>
+                <SwiperSlide>Slide 3</SwiperSlide>
+                <SwiperSlide>Slide 4</SwiperSlide>
+            </Swiper>
             {list.map(item => <p key={item} className={style.text}>test {item}</p>)}
         </div>
     )
