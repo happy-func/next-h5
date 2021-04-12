aee29ab2276f5aa9645cbb833858ee1b48546f2e
zzx <824566462@qq.com>
2021-04-12 17:56:11 +0800

    移除@material-ui/core

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,11 @@
 import React from "react";
-import { Button } from '@material-ui/core';
 import style from '../styles/Home.module.css';
 import BaseLink from "@/components/BaseLink";
 
 function Home({ list }) {
     return (
         <div className={style.page}>
-            <BaseLink src="/test?id=3">
-                <Button color='primary'>test id=2</Button>
-            </BaseLink>
+            <BaseLink src="/test?id=2">test</BaseLink>
             {list.map(item => <p key={item} className={style.text}>test {item}</p>)}
         </div>
     )
